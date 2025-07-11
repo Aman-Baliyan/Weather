e5bd07b76a0e8fc5f40810d2ab38b8557468085a
rahulbhandariitsreal <106346131+rahulbhandariitsreal@users.noreply.github.com>
2025-07-11 19:56:17 +0530

    authentication done

--- src/main/java/com/example/ocean/controller/MailController.java
+++ src/main/java/com/example/ocean/controller/MailController.java
@@ -2,10 +2,7 @@ package com.example.ocean.controller;
 
 import com.example.ocean.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/mail")
